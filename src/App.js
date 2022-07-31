d69ddddbea24c2d53be73da13355b5505e14daca
Lacey-Anne Sanderson <laceyannesanderson@gmail.com>
2022-07-31 17:20:38 -0600

    Add the app header.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './Header/Header';
 import CompactSutra from './CompactSutra/CompactSutra';
 
 function App() {
   return (
     <div className="All-the-Sutras-App">
+      <Header />
       <div className="All-the-Sutras-Content container">
         <div className="list-group list-group-flush">
           <CompactSutra />
---
+++ src/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = (props) => (
+  <h1 className="header">
+  All The Sutras
+  </h1>
+)
+
+export default Header
