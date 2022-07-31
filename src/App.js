5236124ba56d70f6a386c45cdbb8e66c51aabf8f
Lacey-Anne Sanderson <laceyannesanderson@gmail.com>
2022-07-31 17:05:40 -0600

    Expand + theme sutra with example content.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import CompactSutra from './CompactSutra/CompactSutra';
 
 function App() {
   return (
-    <div className="App">
-      <CompactSutra />
+    <div className="All-the-Sutras-App">
+      <div className="All-the-Sutras-Content container">
+        <div className="list-group list-group-flush">
+          <CompactSutra />
+        </div>
+      </div>
     </div>
   );
 }
--- src/CompactSutra/CompactSutra.jsx
+++ src/CompactSutra/CompactSutra.jsx
@@ -2,26 +2,29 @@ import React from 'react';
 import './CompactSutra.css';
 
 const CompactSutra = (props) => (
-    <div className="card-container">
-        <div className="card compact-sutra">
-            <div className="sutra-number">
-                1.1
-            </div>
-            <div className="sanskit-devanagari">
-              अथ योगानुशासनम्
-            </div>
-            <div className="sanskit-latin">
-              atha yogānuśāsanam
-            </div>
-            <div className="core-english-translation">
-              <div className="text">
-                Now, the teachings of yoga.
-              </div>
-              <div className="translator">
-                Chip Hartranft, Arlington Center
-              </div>
-            </div>
+    <div className="compact-sutra list-group-item container text-center">
+      <div className="sutra-core-info row">
+        <div className="col-3">
+          <h2 className="sutra-number">
+              1.1
+          </h2>
         </div>
+        <div className="col">
+          <h2 className="sanskrit-devanagari">
+            अथ योगानुशासनम्
+          </h2>
+          <h2 className="sanskrit-latin">
+            atha yogānuśāsanam
+          </h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="core-english-translation">
+          <div className="text">
+            Now, the teachings of yoga.
+          </div>
+        </div>
+      </div>
     </div>
 )
 
