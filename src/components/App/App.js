68a0bcd6c633ca8e756ece61f801d706651f8feb
Lacey-Anne Sanderson <laceyannesanderson@gmail.com>
2022-08-02 11:26:12 -0600

    Cleanup dir structure.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Header from './Header/Header';
-import SutraListing from './SutraListing/SutraListing';
+import Header from '../Header/Header';
+import SutraListing from '../SutraListing/SutraListing';
 
 function App() {
 
