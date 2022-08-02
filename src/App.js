38e548c41531a27b6d8e81dfc3ba7ca362b65ef9
Lacey-Anne Sanderson <laceyannesanderson@gmail.com>
2022-08-02 10:40:12 -0600

    Moved the listing into it's own component.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './Header/Header';
-import CompactSutra from './CompactSutra/CompactSutra';
+import SutraListing from './SutraListing/SutraListing';
 
 function App() {
+
   return (
     <div className="All-the-Sutras-App">
       <Header />
       <div className="All-the-Sutras-Content container">
-        <div className="list-group list-group-flush">
-          <CompactSutra />
-        </div>
+        <SutraListing />
       </div>
     </div>
   );
---
+++ src/SutraListing/SutraListing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SutraListing.css';
+import CompactSutra from '../CompactSutra/CompactSutra';
+
+const SutraListing = (props) => (
+  <div className="list-group list-group-flush">
+    <CompactSutra />
+  </div>
+)
+
+export default SutraListing
