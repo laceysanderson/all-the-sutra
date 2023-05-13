497f173393d22e5577227cf6959232fbd498c5ea
Lacey-Anne Sanderson <laceyannesanderson@gmail.com>
2023-05-13 09:56:06 -0600

    Expand for bigger screens.

--- src/components/CompactSutra/CompactSutra.jsx
+++ src/components/CompactSutra/CompactSutra.jsx
@@ -5,7 +5,7 @@ export default function CompactSutra(sutra) {
   return (
     <div className="compact-sutra list-group-item container text-center">
       <div className="sutra-core-info row">
-        <div className="col-3 left-side">
+        <div className="col-4 left-side">
           <h2 className="sutra-number">
               {sutra.number}
           </h2>
@@ -20,7 +20,7 @@ export default function CompactSutra(sutra) {
         </div>
       </div>
       <div className="core-english-translation row">
-        <div className="col-3 left-side info">
+        <div className="col-4 left-side info">
           {sutra.author}
         </div>
         <div className="col right-side text">
